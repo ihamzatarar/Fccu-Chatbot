e403775ce1d5cc08bbf4a8e9fc321bbc630cd350
Hamza Tarar <83910939+ihamzatarar@users.noreply.github.com>
2024-06-25 12:55:03 +0500

    get respose api added

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("chat/", views.ChatListCreate.as_view(), name="chat-list"),
     path("chat/delete/<int:pk>/", views.ChatDelete.as_view(), name="delete-chat"),
+    path("get/", views.GetResponseView.as_view(), name="get-response")
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from rest_framework import generics
 from .serializers import UserSerializer, ChatSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .models import Chat
 
 
@@ -34,3 +36,21 @@ class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
+
+
+class GetResponseView(APIView):
+    permission_classes = [AllowAny]
+    def post(self, request, *args, **kwargs):
+        message = request.data.get('message')
+        if message:
+            response = get_chatbot_response(message)
+            return Response({'response': response})
+        return Response({'error': 'Message not provided'}, status=400)
+
+
+# chatbot/minne.py
+def get_chatbot_response(message):
+    # Your logic to process `message` and generate a response
+    return "Sample response from minne.py"
+
